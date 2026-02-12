8e1e9a8cf1a895408272675796dd7c60ef8642a7
Gabriel Claros <gabriel.claros.g@ucb.edu.bo>
2026-02-11 21:53:33 -0800

    Multiplicacion

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,25 @@
-import sumar from "./sumador";
+import {sumar,  multiplicar} from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const sumarBtn = document.querySelector("#sumar-button");
+const multiplicarBtn = document.querySelector("#multiplicar-button");
 const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
+sumarBtn.addEventListener("click", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>Suma: "+ sumar(firstNumber, secondNumber) + "</p>";
 });
+multiplicarBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>Multiplicacion: " + multiplicar(firstNumber, secondNumber) + "</p>";
+});
+
